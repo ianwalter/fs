7d3da64f35465421a1580f79281a5a227febbd2a
Ian Walter <public@iankwalter.com>
2019-03-10 10:23:09 -0400

    Adding #2

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
 import fs from 'fs'
+import Subpub from '@ianwalter/subpub'
 
+const subpub = new Subpub()
 const proxy = new Proxy(fs, {
   get (target, name) {
     const prop = target[name]
 
-    if (typeof prop === 'function' && !name.includes('Sync')) {
-      return (...args) => new Promise((resolve, reject) => {
-        const callback = (err, data) => {
-          if (err) {
-            reject(err)
-          } else {
-            resolve(data)
-          }
+    if (name !== 'sub' && typeof prop === 'function') {
+      return (...args) => {
+        const subs = subpub.getSubs(args[0])
+        let results
+        if (subs && subs.length) {
+          results = Promise.all(subpub.pub(args[0], { name, args }))
         }
 
-        prop.apply(target, args.concat(callback))
-      })
+        if (name.includes('Sync')) {
+          return prop.apply(target, args)
+        } else {
+          return new Promise(async (resolve, reject) => {
+            try {
+              if (results) {
+                resolve((await results).find(result => result !== undefined))
+              } else {
+                prop.apply(target, args.concat((err, data) => {
+                  if (err) {
+                    reject(err)
+                  } else {
+                    resolve(data)
+                  }
+                }))
+              }
+            } catch (err) {
+              reject(err)
+            }
+          })
+        }
+      }
     }
 
     return prop
   }
 })
+
+proxy.sub = subpub.sub.bind(subpub)
 
 export default proxy
--- test/test.js
+++ test/test.js
@@ -1,6 +1,45 @@
 import test from 'ava'
 import fs from '..'
 
-test('reading a file using async/await', async t => {
-  t.is(await fs.readFile('./test/fixtures/hello.txt', 'utf8'), 'Hello!\n')
+const helloPath = './test/fixtures/hello.txt'
+const twitterPath = './test/fixtures/twitter.txt'
+
+test('readFileSync', t => {
+  t.is(fs.readFileSync(helloPath, 'utf8'), 'Hello!\n')
+})
+
+test('readFile using async/await', async t => {
+  t.is(await fs.readFile(helloPath, 'utf8'), 'Hello!\n')
+})
+
+test('access using async/await', async t => {
+  await t.notThrowsAsync(
+    async () => await fs.access(helloPath)
+  )
+})
+
+test.cb('readFileSync with subscription', t => {
+  const unsubscribe = fs.sub(helloPath, action => {
+    t.is(action.name, 'readFileSync')
+    t.is(action.args[0], helloPath)
+    t.is(action.args[1], 'utf8')
+    t.end()
+  })
+  fs.readFileSync(helloPath, 'utf8')
+  unsubscribe()
+})
+
+test('preventing a file read with a subscription', async t => {
+  const errorMsg = 'No distractions!'
+  const unsubscribe = fs.sub(twitterPath, async () => {
+    throw new Error(errorMsg)
+  })
+  await t.throwsAsync(async () => await fs.readFile(twitterPath), errorMsg)
+  unsubscribe()
+})
+
+test('hijacking a file read with a subscription', async t => {
+  const unsubscribe = fs.sub(helloPath, () => 'Hola!')
+  t.is(await fs.readFile(helloPath, 'utf8'), 'Hola!')
+  unsubscribe()
 })
